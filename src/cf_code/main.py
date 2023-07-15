6ed3b75ac9e9d3b19955e2254370d30ab9b5688e
Arseny Ivanov <arsivanov@google.com>
2023-07-15 20:47:31 -0300

    add cloud function code, deploy function

---
+++ src/cf_code/main.py
@@ -0,0 +1,12 @@
+import os
+import functions_framework
+
+from google.cloud import storage
+storage_client = storage.Client()
+
+@functions_framework.http
+def get_blob(request):
+    bucket = storage_client.bucket(os.environ.get('BUCKET'))
+    blob = bucket.blob(os.environ.get('FILE'))
+    resume = blob.download_as_text()
+    return resume
